b11fe0bdd65dc7b827d98578d2a6c42c376bce4f
Katya Weicht <katya.weicht@gmail.com>
2025-07-18 15:06:13 -0600

    Create flask POST request

--- server.py
+++ server.py
@@ -11,6 +11,16 @@ def home():
 def get_inventory():
   return jsonify(inventory)
 
+@app.route("/events", methods = ["POST"])
+def add_inventory():
+  data = request.get_json()
+  new_id = max((item["id"] for item in inventory), default=0) + 1
+  if data and "product" in data and "price" in data and "stock" in data:
+    new_event = {"id": new_id, "product": data["product"], data["price"], data["stock"] }
+    inventory.append(new_event)
+    return jsonify(new_event), 201
+  return jsonify({"Error":"form cannot be blank"}), 400 
+
 @app.after_request
 def add_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
