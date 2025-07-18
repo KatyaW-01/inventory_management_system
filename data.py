aef88f6e1998f1481202a88207a5ae7185db0446
Katya Weicht <katya.weicht@gmail.com>
2025-07-18 14:20:13 -0600

    Update sample data

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 inventory = [
-  {"id": 1, "product": "Iceberg Lettuce"},
-  {"id": 2, "product": "Green Pesto"},
-  {"id": 3, "product": "Spaghetti"},
-  {"id": 4, "product": "Grape Tomatoes"},
-  {"id": 5, "product": "Plums"}
+  {"id": 1, "product": "Iceberg Lettuce", "price": 5.99, "stock": 20},
+  {"id": 2, "product": "Green Pesto", "price": 9.50, "stock": 13},
+  {"id": 3, "product": "Spaghetti", "price": 2.00, "stock": 5},
+  {"id": 4, "product": "Grape Tomatoes", "price": 3.99, "stock": 10},
+  {"id": 5, "product": "Plums", "price": 4.50, "stock": 47}
 ]
