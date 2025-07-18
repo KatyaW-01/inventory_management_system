0cfe5f5ae0d472647ecdaf03df6d728d0ae3dfe6
Katya Weicht <katya.weicht@gmail.com>
2025-07-18 13:34:48 -0600

    Create sample data

---
+++ data.py
@@ -0,0 +1,7 @@
+products = [
+  {"id": 1, "product": "Iceberg Lettuce"},
+  {"id": 2, "product": "Green Pesto"},
+  {"id": 3, "product": "Spaghetti"},
+  {"id": 4, "product": "Grape Tomatoes"},
+  {"id": 5, "product": "Plums"}
+]
